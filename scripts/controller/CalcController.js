d5b7f24f82a0185265f289b2ce3090ce6853b760
Luís F <fnsc.luis@gmail.com>
2019-07-02 19:08:37 -0300

    Correção de dois erros com o uso do ponto (.). A primeira situação a  calculadora permitia a entrada do ponto mais de uma vez. A segunda situação é tentar colocar um número com casas decimais, por exemplo, 3.05, se tornaria 35.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -107,7 +107,12 @@ class CalcController {
     } // end execButton
 
     addPoint() {
+
         let lastOperation = this.getLastOperation();
+
+        if (typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) {
+            return;
+        }
 
         if (this.isOperator(lastOperation) || !lastOperation) {
             this.pushOperation('0.');
@@ -310,7 +315,7 @@ class CalcController {
                 this.pushOperation(value);
             } else {
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseFloat(newValue));
+                this.setLastOperation(newValue);
                 this.setLastNumberToDisplay();
             } // end if
         } // end if
